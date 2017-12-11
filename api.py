e15dad055bdf1d40517f41f4c6fa8ea4d9c52a76
yangsen <1003761102@qq.com>
2017-12-11 20:12:36 +0800

    上传

--- api.py
+++ api.py
@@ -8,10 +8,6 @@ app = Flask(__name__)
 
 
 def get_conn():
-    """
-    Opens a new redis connection if there is none yet for the
-    current application context.
-    """
     if not hasattr(g, 'redisclient'):
         g.redisclient = redis_client()
     return g.redisclient
@@ -19,32 +15,22 @@ def get_conn():
 
 @app.route('/')
 def index():
-    return '<h2>hello,小马哥</h2>'
+    return '<h2>hello,welcome to ip_proxy</h2>'
 
 
 @app.route('/get')
 def get_proxy():
-    """
-    Get a proxy
-    """
     conn = get_conn()
     ip=conn.lget()
     conn.rput(ip)
     return ip
-
-
 @app.route('/count')
 def get_counts():
-    """
-    Get the count of proxies
-    """
     conn = get_conn()
     return str(conn.lenth())
 @app.route('/pop')
 def pop_proxy():
     conn = get_conn()
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
--- test_headers.py
+++ test_headers.py
@@ -1,6 +1,4 @@
 from get_requests import  get_url
-
-
 url='http://www.zhihu.com/question/30692237'
 url1='http://slashman.me'
 html=get_url(url1)
